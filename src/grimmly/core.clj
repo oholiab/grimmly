5930117eb1c85fba9012b989513679387f7f5db4
Matt Carroll <mattc@yelp.com>
2015-10-28 15:51:01 +0000

    It works!

--- src/grimmly/core.clj
+++ src/grimmly/core.clj
@@ -2,7 +2,6 @@
   (:gen-class))
 
 (require '[org.httpkit.server :as serv]
-         '[clojure.string :as string]
          '[pandect.algo.sha1 :as alg])
 
 (def inventory (ref (array-map)))
@@ -14,50 +13,57 @@
   [k, v]
   (dosync  
     (ref-set inventory (if (= inventory-size (count @inventory)) 
-                         ;(assoc (drop-last @inventory) k v) 
                          (assoc (apply array-map (flatten (rest @inventory))) k v)
                          (assoc @inventory k v)))))
 
 (defn shakey
   "Return a short sha from passed in string"
   [s]
-  (string/join "" (take 5 (alg/sha1 s))))
+  (apply str (take 5 (alg/sha1 s))))
+
+(defn req-body-to-str
+  "Takes the stupid httpkit message body format and turns it to a string"
+  [req]
+  (apply str (map char (.bytes (-> req :body)))))
 
 (defn req-to-body-sha
   "Turns the uri from a req into a sha"
   [req]
-  (shakey (string/join "" (rest (str (-> req :body))))))
+  (shakey (req-body-to-str req)))
 
 (defn add-record
   "Adds record to the inventory"
   [req]
-  (println req)
-  (update-inventory (req-to-body-sha req) (apply str (map char (.bytes (-> req :body)))))
-  (println @inventory)
+  (update-inventory (req-to-body-sha req) (req-body-to-str req))
   {:status  200
    :headers {"Content-Type" "text/css"}
    :body (req-to-body-sha req)})
 
+(defn truncate-uri
+  "Remove leading / from string"
+  [s]
+  (apply str (rest s)))
+
 (defn redirect-record
   "Redirects to the given record or 404s"
   [req]
-  (println req)
-  {:status 302
-   :headers {"Location" "https://grimmwa.re"}})
+  (let [resp (get @inventory (truncate-uri (-> req :uri)))]
+    (if (nil? resp)
+      {:status 404}
+      {:status 302
+       :headers {"Location" resp}})))
 
 (defn reply 
-  "Just do a simple http response"
+  "Routes requests"
   [req]
   (let [method (-> req :request-method)]
-    (println method)
     (case method
       :post (add-record req)
       :get  (redirect-record req)
       (redirect-record "404"))))
 
-
 (defn -main
   "Start up the app and keep it going"
   [& args]
-  (println "Starting up...")
+  (prn "App started.")
   (serv/run-server reply {:port 8080}))
